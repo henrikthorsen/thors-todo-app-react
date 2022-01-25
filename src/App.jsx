28cfaa131ac235256d062492c60345cadbc54aef
Henrik Thorsen <hen.thorsen@gmail.com>
2022-01-25 12:50:45 +0100

    added content from JS project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 // export function App() {
 //   return <h1>Hello Hello Hello!</h1>;
 // }
+import "./App.css"; //added by Thor
 
 export const App = () => {
-  return <h1>Hello Hello Hello there!</h1>;
+  return (
+    <div>
+      <header className="flex justify-content-space-between">
+        <h1><span className="color-gray">Grey </span> <span className="color-salmon">Salmon</span> Todos</h1>
+        <button id="btn-change-mode" className="button background-color-inactive-button">
+          Dark mode
+        </button>
+		  </header>
+      <div id="subheader" className="flex justify-content-flex-end">
+        <button id="btn-filter-all" className="button background-color-active-button">
+          All
+        </button>
+        <button id="btn-filter-undone" className="button background-color-inactive-button">
+          Undone
+        </button>
+        <button id="btn-filter-done" className="button background-color-inactive-button">
+          Done
+        </button>
+      </div>
+      <main>
+        <div id="list-wrapper">
+          <ul id="todos">
+          </ul>
+        </div>
+        <div id="input-field" className="flex justify-content-space-between align-items-center">
+          <input id="todo-title-input" type="text"
+          placeholder="Enter todo" autofocus/>
+          <button id="btn-add-todo">
+            Add
+          </button>
+        </div>
+      </main>
+    </div>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import React from "react"; //added by Thor
 import ReactDOM from "react-dom";
+import "./index.css"; //added by Thor
 import { App } from "./App";
 
 const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+
+ReactDOM.render(
+    <App />, 
+    app
+);
